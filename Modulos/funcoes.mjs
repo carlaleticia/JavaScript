82cf88757a71c1931918ed889c460d622709134f
Carla <carlaleticia.gf@gmail.com>
2022-07-05 15:56:38 -0300

    first commit

---
+++ Modulos/funcoes.mjs
@@ -0,0 +1,18 @@
+function showAge(name, age) {
+    return `The age of ${name} is ${age}`;
+    
+}
+
+function showCity(name, city) {
+    return `The city of ${name} is ${city}`;
+    
+}
+
+function showHobby(name, hobby) {
+    return `${hobby} is ${name}'s hobby.`;
+    
+}
+
+export{
+    showAge, showCity, showHobby
+};
---
+++ Modulos/main.mjs
@@ -0,0 +1,5 @@
+import {showAge, showCity, showHobby} from "./funcoes.mjs";
+
+console.log(showAge('Carla', 26));
+console.log(showCity('Carla', 'Itatiba'));
+console.log(showHobby('Carla', 'travel'));
